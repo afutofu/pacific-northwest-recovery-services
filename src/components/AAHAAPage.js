ffd86d3da6c9b28891b8fe91a701a66dfe1bfb54
afutofu <theafuza@gmail.com>
2022-09-14 15:56:37 +0700

    Added more content & styling into subsections

--- src/components/AAHAAPage.js
+++ src/components/AAHAAPage.js
@@ -1,15 +1,42 @@
 import "../styles/AAHAAPage.css";
+import logo from "../assets/aahaa-logo.png";
 
 const AAHAAPage = () => {
   return (
     <section id="aahaa">
-      <h1>Alcoholics & Addicts Helping Alcoholics & Addicts</h1>
+      <navbar>
+        <img src={logo} />
+      </navbar>
+      <div className="container">
+        <h3>About Us</h3>
+        <p>
+          AAHAA Supportive Housing is a community of men and women in recovery.
+          Through group conscience, fellowship, a 12-step program, and faith -
+          we work towards a common goal.
+        </p>
+      </div>
+      <div className="container">
+        <h3>Services We Provide</h3>
+        <p>Individual Counseling Services</p>
+        <p>Life Skills Training & Seminars</p>
+        <p>Case Management</p>
+        <p>Food</p>
+        <p>Utilities</p>
+        <p>Local & Long Distance Telephone</p>
+        <p>Internet Access</p>
+        <p>Laundry</p>
+        <p>Furnishings (including bed & dressers)</p>
+        <p>Linen and Bath</p>
+        <p>Easy Access to Public Transportation</p>
+      </div>
+
       <a
+        id="aahaa-button"
         href="https://www.aahaasupportivehousing.com"
         target="_blank"
         rel="noopener noreferrer"
       >
-        More information here
+        Full information and services here
       </a>
     </section>
   );
--- src/components/WalkPage.js
+++ src/components/WalkPage.js
@@ -1,15 +1,45 @@
 import "../styles/WalkPage.css";
+import logo from "../assets/awtf-logo.png";
 
 const WalkPage = () => {
   return (
     <section id="awtf">
-      <h1>A Walk To Freedom</h1>
+      <navbar>
+        <img src={logo} />
+      </navbar>
+      <div className="container">
+        <h3>About Us</h3>
+        <p>
+          We help support the health, well-being and healing of individuals and
+          families by making a diffrerence in the lives of the people we
+          interact with each day.
+        </p>
+      </div>
+      <div className="container">
+        <h3>Services We Provide</h3>
+        <p>Intensive Outpatient</p>
+        <p>Relapse Prevention</p>
+        <p>Outpatient</p>
+        <p>Alcohol, Drug information School</p>
+        <p>Aftercare</p>
+        <p>Consumer Awareness</p>
+        <p>Deferred Prosecution</p>
+        <p>Family Law Assessments</p>
+        <p>Drug and Alcohol Assessments</p>
+        <p>Housing Referrals</p>
+        <p>Employment Referrals</p>
+        <p>Peer Support</p>
+        <p>Crisis Help</p>
+        <p>DUI Assessments</p>
+      </div>
+
       <a
+        id="awtf-button"
         href="https://www.aahaasupportivehousing.com"
         target="_blank"
         rel="noopener noreferrer"
       >
-        More information here
+        Full information and services here
       </a>
     </section>
   );
