d9b1d438658488201f946061f22341eab954fccc
afutofu <theafuza@gmail.com>
2022-09-16 20:20:31 +0700

    Change container width on HomePage on hover

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,48 @@
+import { useState } from "react";
+
 import "../styles/Home.css";
 
+import awtfLogo from "../assets/awtf-logo.png";
+import aahaaLogo from "../assets/aahaa-logo.png";
+
 const HomePage = () => {
+  const [showAwtf, setShowAwtf] = useState(false);
+  const [showAahaa, setShowAahaa] = useState(false);
+
   return (
     <section id="home">
-      <a id="awtf-container" href="/awtf">
+      <a
+        id="awtf-container"
+        href="/awtf"
+        className={
+          showAwtf
+            ? "container-expand"
+            : showAahaa
+            ? "container-shrink"
+            : "container-normal"
+        }
+        onMouseOver={() => setShowAwtf(true)}
+        onMouseLeave={() => setShowAwtf(false)}
+      >
         <div className="container">
-          <h1>A Walk To Freedom</h1>
+          <img id="awtf-logo" src={awtfLogo} alt="A Walk To Freedom" />
         </div>
       </a>
-      <a id="aahaa-container" href="/aahaa">
+      <a
+        id="aahaa-container"
+        href="/aahaa"
+        className={
+          showAahaa
+            ? "container-expand"
+            : showAwtf
+            ? "container-shrink"
+            : "container-normal"
+        }
+        onMouseOver={() => setShowAahaa(true)}
+        onMouseLeave={() => setShowAahaa(false)}
+      >
         <div className="container">
-          <h1>Alcoholics & Addicts Helping Alcoholics & Addicts</h1>
+          <img id="aahaa-logo" src={aahaaLogo} alt="AAHAA" />
         </div>
       </a>
     </section>
