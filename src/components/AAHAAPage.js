0e806bbc8c84333df36351a59c7a3a49516fa700
afutofu <theafuza@gmail.com>
2022-09-21 22:48:30 +0700

    Implemented design for aahaa page

--- src/components/AAHAAPage.js
+++ src/components/AAHAAPage.js
@@ -1,43 +1,75 @@
 import "../styles/AAHAAPage.css";
 import logo from "../assets/images/aahaa-logo.png";
+import bg from "../assets/images/aahaa-home-bg.jpg";
+import facebookIcon from "../assets/icons/iconmonstr-facebook-white.png";
+
+import TestimonialSelector from "./TestimonialSelector";
 
 const AAHAAPage = () => {
   return (
     <section id="aahaa">
-      <navbar>
-        <img src={logo} alt="AAHAA" />
-      </navbar>
-      <div className="container">
-        <h3>About Us</h3>
+      <img className="bg-image" src={bg} alt="path going through forest" />
+      <header>
+        <div className="icons">
+          <a
+            href="https://www.facebook.com/aahaasoberliving"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebookIcon} alt="facebook logo" />
+          </a>
+        </div>
+        <img className="title" src={logo} alt="A Walk To Freedom" />
+        <p>Tel: +1 (253) 246-1613</p>
+      </header>
+      <div className="container about">
+        {/* <h3>About Us</h3> */}
         <p>
           AAHAA Supportive Housing is a community of men and women in recovery.
-          Through group conscience, fellowship, a 12-step program, and faith -
-          we work towards a common goal.
+          Through <strong>group conscience</strong>, <strong>fellowship</strong>
+          , a 12-step program, and <strong>faith</strong> - we work towards a
+          common goal. A new way of life is possible, one just has to be{" "}
+          <strong>willing</strong> to try something new and have a willingness
+          to embrace <strong>change</strong>.
         </p>
+        <a
+          id="aahaa-button"
+          href="https://www.aahaasupportivehousing.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Full information and services here
+        </a>
       </div>
-      <div className="container">
+      <div className="container services">
         <h3>Services We Provide</h3>
-        <p>Individual Counseling Services</p>
-        <p>Life Skills Training & Seminars</p>
-        <p>Case Management</p>
-        <p>Food</p>
-        <p>Utilities</p>
-        <p>Local & Long Distance Telephone</p>
-        <p>Internet Access</p>
-        <p>Laundry</p>
-        <p>Furnishings (including bed & dressers)</p>
-        <p>Linen and Bath</p>
-        <p>Easy Access to Public Transportation</p>
+        <ul>
+          <li>Individual Counseling Services</li>
+          <li>Life Skills Training & Seminars</li>
+          <li>Case Management</li>
+          <li>Food</li>
+          <li>Utilities</li>
+          <li>Local & Long Distance Telephone</li>
+          <li>Internet Access</li>
+          <li>Laundry</li>
+          <li>Furnishings (including bed & dressers)</li>
+          <li>Linen and Bath</li>
+          <li>Easy Access to Public Transportation</li>
+        </ul>
       </div>
-
-      <a
-        id="aahaa-button"
-        href="https://www.aahaasupportivehousing.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Full information and services here
-      </a>
+      <div className="container team">
+        <h3>Testimonials</h3>
+        <TestimonialSelector />
+        <div className="separator" />
+        <a
+          id="aahaa-button"
+          href="https://www.aahaasupportivehousing.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Full information and services here
+        </a>
+      </div>
     </section>
   );
 };
---
+++ src/components/TestimonialSelector.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import "../styles/TestimonialSelector.css";
+
+const testimonials = [
+  {
+    name: "Jacob",
+    desc: "I have been able to start and have 2 successful businesses going. With the support and help from AAHAA I have gained so much knowledge and support to stay sober and away from drugs and all negative parts of my past. I know I would not be who I am today or in the position I am in without the support of AAHAA, I would not be so happy. Thank you for all your support!",
+  },
+  {
+    name: "Jose",
+    desc: "This program has been a blessing to me, my family, and many others who have had to fight the battle of chemical dependency. I would encourage anyone who is interested in help for themselves or others, to reach out and contact AAHAA Clean and Sober Living — you won’t regret it!",
+  },
+  {
+    name: `Reese`,
+    desc: "Cannot say enough good things about this Company. I haven’t come across one person, including ones that had to leave the program, say anything but positive things about staff, Management, Case Workers, or the Owner. They have saved many lives. including mine for sure! There are programs that AAHAA works with to get your Rent paid for, REACH being one of them!",
+  },
+
+  {
+    name: "Nadine M.",
+    desc: "I have more responsibility with the program, and I also now have a great job working downtown Seattle. AAHAA has given me the tools I needed to get my life and self-esteem back. I am truly humbled and grateful to this program.",
+  },
+];
+
+const TestimonialSelector = () => {
+  const [selected, setSelected] = useState(0);
+
+  return (
+    <div id="testimonial-selector">
+      <h2 className="name">{testimonials[selected].name} </h2>
+      <div className="content">
+        <p className="desc">{testimonials[selected].desc}</p>
+      </div>
+      <div className="options">
+        {testimonials.map((testimonial, index) => {
+          return (
+            <h3
+              alt="person"
+              className={`option ${selected === index && "selected"}`}
+              onClick={() => setSelected(index)}
+            >
+              {testimonial.name}
+            </h3>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialSelector;
